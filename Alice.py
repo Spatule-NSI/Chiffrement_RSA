6aa6566a1d35c40078f9bd2532838bce34266953
Spatule-NSI <79956892+Spatule-NSI@users.noreply.github.com>
2021-05-26 07:35:10 +0400

    Add files via upload

--- Alice.py
+++ Alice.py
@@ -1,12 +1,7 @@
-from serveur import key, chiffre
-from Test import premier, cut, pgcd, pgcde
-from Utilisateur import dechiffre
-import numpy as np
 import socket, sys
 
-hote = '127.0.0.1'
-port = 33000
-k = key()
+hote = ''
+port = 
 
 serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -18,24 +13,15 @@ except socket.error:
     sys.exit()
 print("Connexion établie avec Bob.")
 
-
-
 msgServeur = serveur.recv(1024).decode()
 
 while 1:
     if msgServeur.upper() == "FIN" or msgServeur =="":
         break
-    if msgServeur == "Vous êtes connecté avec Bob.":
-        print("Bob>",msgServeur)
-    else:
-        print("Bob>")
+    print("Serveur>", msgServeur)
     msgClient = input("Alice> ")
-    msgClient = chiffre(k["priv"][0],k["priv"][1],msgClient)
-    ''.join(msgClient)
     serveur.send(msgClient.encode())
     msgServeur = serveur.recv(1024).decode()
-    print(msgServeur)
-    msgServeur = cut(msgServeur,2)
-    msgServeur = dechiffre(pub["pub"][0],pub["pub"][1],msgServeur)
+    
 print("Connexion interrompue.")
 serveur.close()
--- Bob.py
+++ Bob.py
@@ -1,13 +1,7 @@
-from serveur import key, chiffre
-from Test import premier, cut, pgcd, pgcde
-from Utilisateur import dechiffre
-import numpy as np
 import socket, sys
-
-hote = '127.0.0.1'
-port = 33000
-k = key()
-
+hote = ''
+port = 
+counter = 0
 
 serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -17,28 +11,28 @@ try:
 except socket.error:
     print("La liaison avec Alice a échoué.")
     
-print("Serveur prêt, en attente de requêtes ...")
-serveur.listen(1)
-connexion, adresse = serveur.accept()
-print("Alice est connectée, adresse IP %s, port %s" % (adresse[0], adresse[1]))
-
-
-
-msgServeur = "Vous êtes connecté avec Bob."
-connexion.send(msgServeur.encode())
-msgClient = connexion.recv(1024).decode()
 while 1:
-    print("Alice>")
-    if msgClient.upper() == "FIN" or msgClient =="":
+    print("Serveur prêt, en attente de requêtes ...")
+    serveur.listen(2)
+    
+    connexion, adresse = serveur.accept()
+    counter +=1
+    print("Client connecté, adresse IP %s, port %s" % (adresse[0], adresse[1]))
+    
+    msgServeur = " Vous êtes connecté au serveur Chiffrement RSA !"
+    connexion.send(msgServeur.encode())
+    msgClient = connexion.recv(1024).decode()
+    while 1:
+        print("Alice>", msgClient)
+        if msgClient.upper() == "FIN" or msgClient =="":
+            break
+        msgServeur = input("Bob> ")
+        connexion.send(msgServeur.encode())
+        msgClient = connexion.recv(1024).decode()
+    connexion.send("fin".encode())
+    print("Connexion interrompue.")
+    connexion.close()
+    
+    ch = input("<R>ecommencer <T>erminer ?")
+    if ch.upper() == 'T':
         break
-    msgServeur = input("Bob> ")
-    msgServeur = chiffre(k["priv"][0],k["priv"][1],msgServeur)
-    ''.join(msgServeur)
-    serveur.send(msgClient.encode())
-    msgServeur = msgClient.recv(1024).decode()
-    print(msgClient)
-    msgClient = cut(msgClient, 2)
-    msgClient = dechiffre(pub["pub"][0],pub["pub"][1],msgClient)
-connexion.send("fin".encode())
-print("Connexion interrompue.")
-connexion.close()
